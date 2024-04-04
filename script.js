40898a554a3a0d12a9ac786e3965ab1ff6471703
Giorgiana353 <146415215+Giorgiana353@users.noreply.github.com>
2024-04-04 18:03:19 +0300

    script

---
+++ script.js
@@ -0,0 +1,261 @@
+function setup() {
+    createCanvas(430, 250);
+}
+function draw() {
+    background("lightblue");
+    drawsquare();
+}
+function drawsquare() {
+    //1
+    stroke("black")
+    fill("red");
+    square(10, 10, 50);
+    fill("black");
+    textSize(20);
+    textAlign(CENTER, CENTER);
+    text("1", 10 + 25, 10 + 25);
+
+    //2
+    stroke("black")
+    fill("#99ffcc");
+    square(70, 10, 50);
+    fill("black");
+    textSize(20);
+    textAlign(CENTER, CENTER);
+    text("2", 70 + 25, 10 + 25);
+
+    //3
+    stroke("black")
+    fill("#4db8ff");
+    square(130, 10, 50);
+    fill("black");
+    textSize(20);
+    textAlign(CENTER, CENTER);
+    text("3", 130 + 25, 10 + 25);
+
+    //4
+    stroke("black")
+    fill("#ffff66");
+    square(190, 10, 50);
+    fill("black");
+    textSize(20);
+    textAlign(CENTER, CENTER);
+    text("4", 190 + 25, 10 + 25);
+
+    //5
+    stroke("black")
+    fill("#666600");
+    square(250, 10, 50);
+    fill("black");
+    textSize(20);
+    textAlign(CENTER, CENTER);
+    text("5", 250 + 25, 10 + 25);
+
+    //6
+    stroke("black")
+    fill("#990000");
+    square(310, 10, 50);
+    fill("black");
+    textSize(20);
+    textAlign(CENTER, CENTER);
+    text("6", 310 + 25, 10 + 25);
+
+    //7
+    stroke("black")
+    fill("#ff33ff");
+    square(370, 10, 50);
+    fill("black");
+    textSize(20);
+    textAlign(CENTER, CENTER);
+    text("7", 370 + 25, 10 + 25);
+
+    //8
+    stroke("black")
+    fill("#b3b3ff");
+    square(10, 70, 50);
+    fill("black");
+    textSize(20);
+    textAlign(CENTER, CENTER);
+    text("8", 10 + 25, 70 + 25);
+
+    //9
+    stroke("black")
+    fill("#ff66a3");
+    square(70, 70, 50);
+    fill("black");
+    textSize(20);
+    textAlign(CENTER, CENTER);
+    text("9", 70 + 25, 70 + 25);
+
+    //10
+    stroke("black")
+    fill("#ffff99");
+    square(130, 70, 50);
+    fill("black");
+    textSize(20);
+    textAlign(CENTER, CENTER);
+    text("10", 130 + 25, 70 + 25);
+
+    //11
+    stroke("black")
+    fill("#003300");
+    square(190, 70, 50);
+    fill("black");
+    textSize(20);
+    textAlign(CENTER, CENTER);
+    text("11", 190 + 25, 70 + 25);
+
+    //12
+    stroke("black")
+    fill("#9900cc");
+    square(250, 70, 50);
+    fill("black");
+    textSize(20);
+    textAlign(CENTER, CENTER);
+    text("12", 250 + 25, 70 + 25);
+
+    //13
+    stroke("black")
+    fill("#99ff99");
+    square(310, 70, 50);
+    fill("black");
+    textSize(20);
+    textAlign(CENTER, CENTER);
+    text("13", 310 + 25, 70 + 25);
+
+    //14
+    stroke("black")
+    fill("#000066");
+    square(370, 70, 50);
+    fill("black");
+    textSize(20);
+    textAlign(CENTER, CENTER);
+    text("14", 370 + 25, 70 + 25);
+
+    //15
+    stroke("black")
+    fill("green");
+    square(10, 130, 50);
+    fill("black");
+    textSize(20);
+    textAlign(CENTER, CENTER);
+    text("15", 10 + 25, 130 + 25);
+
+    //16
+    stroke("black")
+    fill("gray");
+    square(70, 130, 50);
+    fill("black");
+    textSize(20);
+    textAlign(CENTER, CENTER);
+    text("16", 70 + 25, 130 + 25);
+
+    //17
+    stroke("black")
+    fill("yellow");
+    square(130, 130, 50);
+    fill("black");
+    textSize(20);
+    textAlign(CENTER, CENTER);
+    text("17", 130 + 25, 130 + 25);
+
+    //18
+    stroke("black")
+    fill("lightgreen");
+    square(190, 130, 50);
+    fill("black");
+    textSize(20);
+    textAlign(CENTER, CENTER);
+    text("18", 190 + 25, 130 + 25);
+
+    //19
+    stroke("black")
+    fill("orange");
+    square(250, 130, 50);
+    fill("black");
+    textSize(20);
+    textAlign(CENTER, CENTER);
+    text("19", 250 + 25, 130 + 25);
+
+    //20
+    stroke("black")
+    fill("lightcoral");
+    square(310, 130, 50);
+    fill("black");
+    textSize(20);
+    textAlign(CENTER, CENTER);
+    text("20", 310 + 25, 130 + 25);
+
+    //21
+    stroke("black")
+    fill("#ccffcc");
+    square(370, 130, 50);
+    fill("black");
+    textSize(20);
+    textAlign(CENTER, CENTER);
+    text("21", 370 + 25, 130 + 25);
+
+    //22
+    stroke("black")
+    fill("crimson");
+    square(10, 190, 50);
+    fill("black");
+    textSize(20);
+    textAlign(CENTER, CENTER);
+    text("22", 10 + 25, 190 + 25);
+
+    //23
+    stroke("black")
+    fill("white");
+    square(70, 190, 50);
+    fill("black");
+    textSize(20);
+    textAlign(CENTER, CENTER);
+    text("23", 70 + 25, 190 + 25);
+
+    //24
+    stroke("black")
+    fill("wheat");
+    square(130, 190, 50);
+    fill("black");
+    textSize(20);
+    textAlign(CENTER, CENTER);
+    text("24", 130 + 25, 190 + 25);
+
+    //25
+    stroke("black")
+    fill("purple");
+    square(190, 190, 50);
+    fill("black");
+    textSize(20);
+    textAlign(CENTER, CENTER);
+    text("25", 190 + 25, 190 + 25);
+
+    //26
+    stroke("black")
+    fill("brown");
+    square(250, 190, 50);
+    fill("black");
+    textSize(20);
+    textAlign(CENTER, CENTER);
+    text("26", 250 + 25, 190 + 25);
+
+    //27
+    stroke("black")
+    fill("hotpink");
+    square(310, 190, 50);
+    fill("black");
+    textSize(20);
+    textAlign(CENTER, CENTER);
+    text("27", 310 + 25, 190 + 25);
+
+    //28
+    stroke("black")
+    fill("beige");
+    square(370, 190, 50);
+    fill("black");
+    textSize(20);
+    textAlign(CENTER, CENTER);
+    text("28", 370 + 25, 190 + 25);
+
+}
